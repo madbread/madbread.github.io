add41e2ab595905686813ada3f5ed9e105060206
Michael Slater <slatron@gmail.com>
2014-07-16 16:39:06 -0500

    updates large images

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
 ( function($) {
 	
 	// Setup variables
-	$window = $(window);
 	$body = $('body');
   $nav = $body.find('nav');
   $banner = $body.find('#banner');
+  $social = $body.find('#social');
+  $player = $body.find('#player');
+  $shows = $body.find('#shows');
 	
   //Load Images 
 	$body.imagesLoaded( function() {
@@ -20,12 +22,25 @@
 		  $body.removeClass('loading').addClass('loaded');
 
       setTimeout(function() { $nav.addClass('active'); }, 1000);
-
       setTimeout(function() { $banner.addClass('active'); }, 1400);
+      setTimeout(function() { $social.addClass('active'); }, 1600);
 		  
 		}, 800);
 	});
 
+  $('.show-player').on('click', function(e) {
+    e.preventDefault();
+    $player.toggleClass('active');
+  });
+
+  $('.show-shows').on('click', function(e) {
+    e.preventDefault();
+    $shows.toggleClass('active');
+    $social.toggleClass('active');
+    $banner.toggleClass('active');
+  });
+
+  $(".fancybox").fancybox();
 
 		
 })(jQuery);
