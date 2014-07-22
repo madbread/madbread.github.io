36f58f09861ac1110d05f084abfac5c4b85566e0
Michael Slater <slatron@gmail.com>
2014-07-21 22:49:37 -0500

    setting up booking area

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@
 ( function($) {
 	
 	// Setup variables
-	$body   = $('body');
-  $nav    = $body.find('nav');
-  $banner = $body.find('#banner');
-  $social = $body.find('#social');
-  $player = $body.find('#player');
-  $shows  = $body.find('#shows');
+	$body     = $('body');
+  $nav      = $body.find('nav');
+  $banner   = $body.find('#banner');
+  $social   = $body.find('#social');
+  $player   = $body.find('#player');
+  $shows    = $body.find('#shows');
+  $booking  = $body.find('#booking');
 	
   //Load Images 
 	$body.imagesLoaded( function() {
@@ -28,16 +29,30 @@
 		}, 800);
 	});
 
+  // Player trigger
   $('.show-player').on('click', function(e) {
     e.preventDefault();
     $player.toggleClass('active');
   });
 
+  // show trigger
   $('.show-shows').on('click', function(e) {
     e.preventDefault();
     $shows.toggleClass('active');
     $social.toggleClass('active');
     $banner.toggleClass('active');
+  });
+
+  // show booking info
+  $('#slide-4').waypoint({
+    handler: function(dir) {
+      if(dir === 'down') {
+        $booking.addClass('active');
+        $player.removeClass('active');
+      } else {
+        $booking.removeClass('active');
+      }
+    }
   });
 
   $(".fancybox").fancybox();
